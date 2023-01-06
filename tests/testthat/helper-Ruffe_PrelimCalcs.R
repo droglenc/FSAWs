9cf811d5d98e23efe1776f7905679f544f27caa1
Derek Ogle <DerekOgle51@gmail.com>
2023-01-06 13:20:32 -0600

    Updated loop in RuffeWs example and tests

--- tests/testthat/helper-Ruffe_PrelimCalcs.R
+++ tests/testthat/helper-Ruffe_PrelimCalcs.R
@@ -1,5 +1,4 @@
 data(RuffeWs)
-
 ## Create log10 TL and Wt
 RuffeWs$logtl <- log10(RuffeWs$tl)
 RuffeWs$logwt <- log10(RuffeWs$wt)
@@ -7,38 +6,34 @@ RuffeWs$logwt <- log10(RuffeWs$wt)
 ## Isolate development and validation data sets
 rWs.d <- droplevels(subset(RuffeWs,use=="develop"))
 str(rWs.d)
-
 rWs.v <- droplevels(subset(RuffeWs,use=="validate"))
 str(rWs.v)
 
-## Compute regression results for all data sets in development set
-# First, a function to loop through the Wt vs. TL regressions
-loopreg <- function(d,alpha=0.05) {
-  reg.nums <- sort(unique(d$regrnum))
-  n <- minTL <- maxTL <- minWT <- maxWT <- r2 <- loga <- b <- numeric(length(reg.nums))
-  reg.lbls <- country <- character(length(reg.nums))
-  sigb3 <- logical(length(reg.nums))
-  for (i in 1:length(reg.nums)) {
-    df <- droplevels(subset(d,regrnum==reg.nums[i]))
-    reg.lbls[i] <- levels(df$loc)
-    minTL[i] <- min(df$tl)
-    maxTL[i] <- max(df$tl)
-    minWT[i] <- min(df$wt)
-    maxWT[i] <- max(df$wt)
-    lw.lm <- lm(logwt~logtl,data=df)
-    n[i] <- dim(lw.lm$model)[1]
-    r2[i] <- summary(lw.lm)$r.squared
-    loga[i] <- coef(lw.lm)[1]
-    b[i] <- coef(lw.lm)[2]
-  }
-  data.frame(reg.lbls,n,minTL,maxTL,minWT,maxWT,loga,b,sigb3,r.sq=round(r2,3))
+## Loop through all regressions
+### First make a function that performs a regression on one regrnum
+###   and creates a data.frame of desired results
+indivreg <- function(d,alpha=0.05) {
+  tmp.lm <- lm(logwt~logtl,data=d)
+  tmp.smry <- summary(tmp.lm)
+  tmp.cf <- tmp.smry$coefficients[,"Estimate"]
+  tmp.bt3 <- abs((tmp.cf[[2]]-3)/tmp.smry$coefficients["logtl","Std. Error"])
+  tmp.bp3 <- 2*stats::pt(tmp.bt3,df=tmp.smry$df[2],lower.tail=FALSE)
+  data.frame(regrnum=unique(d$regrnum),reg.lbls=unique(d$loc),
+             minTL=min(d$tl,na.rm=TRUE),maxTL=max(d$tl,na.rm=TRUE),
+             minWT=min(d$wt,na.rm=TRUE),maxWT=max(d$wt,na.rm=TRUE),
+             n=dim(tmp.lm$model)[1],r2=tmp.smry$r.squared,
+             loga=tmp.cf[[1]],b=tmp.cf[[2]],sigb3=tmp.bp3<alpha)
 }
 
-# Second, do the looping and view the results
-reg.d <- loopreg(rWs.d)
+### Second, split the data frame into a list where each item is one regrnum
+tmp <- split(rWs.d,as.factor(rWs.d$regrnum))
+### Third, apply the indivreg function to each item in the list
+reg.d <- as.data.frame(do.call(rbind,lapply(tmp,FUN=indivreg)))
+### View (partially) the results
 head(reg.d)
 
-# Third, fit the five different Ws methods
+
+# Fit the five different Ws methods
 ruf75.rlp <- rlp(reg.d$loga,reg.d$b,min=60,max=210,w=10)
 ruf50.rlp <- rlp(reg.d$loga,reg.d$b,min=60,max=210,w=10,probs=0.5)
 
